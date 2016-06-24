5eca1c4dfa564e16bb36449aadfaa65477366977
Loredana Crusoveanu <loredana.crusoveanu@yahoo.com>
2016-06-24 21:50:35 +0300

    rename usercontroller

--- src/main/java/org/baeldung/web/controller/UserController.java
+++ src/main/java/org/baeldung/web/controller/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class UsersController {
+public class UserController {
     
     @Autowired
     ActiveUserStore activeUserStore;
