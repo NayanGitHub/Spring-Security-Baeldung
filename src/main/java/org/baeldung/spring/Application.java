b846fe3ca3043f3fa5fc9bbd01279e331502caa6
eugenp <hanriseldon@gmail.com>
2016-10-22 10:24:45 +0300

    Spring Boot upgrade

--- src/main/java/org/baeldung/spring/Application.java
+++ src/main/java/org/baeldung/spring/Application.java
@@ -2,7 +2,7 @@ package org.baeldung.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.context.request.RequestContextListener;
 
