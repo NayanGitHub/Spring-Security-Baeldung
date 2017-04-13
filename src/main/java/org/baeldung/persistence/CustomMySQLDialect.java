69f22514a597a326e573d4eae4bfbb7f1dfad8e3
DOHA <dohae243@gmail.com>
2017-04-13 13:06:40 +0200

    fix SchemaExport issue

---
+++ src/main/java/org/baeldung/persistence/CustomMySQLDialect.java
@@ -0,0 +1,10 @@
+package org.baeldung.persistence;
+
+import org.hibernate.dialect.MySQLDialect;
+
+public class CustomMySQLDialect extends MySQLDialect {
+    @Override
+    public boolean dropConstraints() {
+        return false;
+    }
+}
