802f51add7ace2c1ec93d5196765ef7c41da479e
Loredana Crusoveanu <loredana.crusoveanu@yahoo.com>
2018-01-10 22:37:02 +0200

    add registration validationt est

--- src/test/java/org/baeldung/test/RegistrationControllerIntegrationTest.java
+++ src/test/java/org/baeldung/test/RegistrationControllerIntegrationTest.java
@@ -5,28 +5,43 @@ import org.baeldung.persistence.model.User;
 import org.baeldung.persistence.model.VerificationToken;
 import org.baeldung.spring.TestDbConfig;
 import org.baeldung.spring.TestIntegrationConfig;
+import org.baeldung.web.dto.UserDto;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.json.JsonContentAssert;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.context.WebApplicationContext;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = { Application.class, TestDbConfig.class, TestIntegrationConfig.class }, webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -44,18 +59,23 @@ public class RegistrationControllerIntegrationTest {
 
     @Before
     public void setUp() {
-        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+            .build();
 
         User user = new User();
-        user.setEmail(UUID.randomUUID().toString() + "@example.com");
-        user.setPassword(UUID.randomUUID().toString());
+        user.setEmail(UUID.randomUUID()
+            .toString() + "@example.com");
+        user.setPassword(UUID.randomUUID()
+            .toString());
         user.setFirstName("First");
         user.setLastName("Last");
 
         entityManager.persist(user);
-        token = UUID.randomUUID().toString();
+        token = UUID.randomUUID()
+            .toString();
         VerificationToken verificationToken = new VerificationToken(token, user);
-        verificationToken.setExpiryDate(Date.from(Instant.now().plus(2, ChronoUnit.DAYS)));
+        verificationToken.setExpiryDate(Date.from(Instant.now()
+            .plus(2, ChronoUnit.DAYS)));
 
         entityManager.persist(verificationToken);
 
@@ -72,5 +92,22 @@ public class RegistrationControllerIntegrationTest {
         resultActions.andExpect(status().is3xxRedirection());
         resultActions.andExpect(model().attribute("message", "Your account verified successfully"));
         resultActions.andExpect(view().name("redirect:/login?lang=en"));
+    }
+
+    @Test
+    public void testRegistrationValidation() throws Exception {
+
+        final MultiValueMap<String, String> param = new LinkedMultiValueMap<>();
+        param.add("firstName", "");
+        param.add("lastName", "");
+        param.add("email", "");
+        param.add("password", "");
+        param.add("matchingPassword", "");
+
+        ResultActions resultActions = this.mockMvc.perform(post("/user/registration").params(param));
+        resultActions.andExpect(status().is(400));
+        resultActions.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+            .andExpect(jsonPath("$.error", is("InvaliduserDto")))
+            .andExpect(jsonPath("$.message", containsString("{\"field\":\"lastName\",\"defaultMessage\":\"Length must be greater than 1\"}")));
     }
 }
